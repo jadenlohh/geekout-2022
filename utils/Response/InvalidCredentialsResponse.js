bf9384fc584c3e52499159efd28208d0880318d8
Nabil Ridhwan <57290483+nabilridhwan@users.noreply.github.com>
2022-06-14 14:29:56 +0800

    fix: :ambulance: Changes the status code for Invalid Credentials from 400 to 401

--- utils/Response/InvalidCredentialsResponse.js
+++ utils/Response/InvalidCredentialsResponse.js
@@ -2,7 +2,7 @@ const Response = require("./Response");
 
 class InvalidCredentialsResponse extends Response {
     constructor(data) {
-        super(400, false, data);
+        super(401, false, data);
     }
 }
 
