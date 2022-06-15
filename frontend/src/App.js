12d863c4d2e749156c48d83b94a480eddd8bfe37
jadenlohh <jadenloh24@gmail.com>
2022-06-15 12:50:14 +0800

    feat: create navigation bar and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+  
 
 function App() {
-    useEffect(() => {
-        // ! This is to get the data from the API, and console log it into your console (check it out!)
-        axios
-            .get("http://localhost:3001/data", {
-                method: "GET",
-                params: {
-                    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJ0ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTY1NTIyMjc3MH0.Kuc6dDkrE5IR-IiTn3NSxQq7Lv6X8uAmm9VFTVgGOh4",
-                },
-            })
-            .then((res) => console.log(res.data));
-    }, []);
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <Routes>
+                <Route path='/login' element={ <Login /> } />
+            </Routes>
+        </Router>
     );
 }
-
+  
 export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Login() {
+    return (
+        <>
+            <h1>This is the login page</h1>
+        </>
+    )
+};
+  
+export default Login;
