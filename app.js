133c9161b470d71c25a4538dd476e43e06b22574
Nabil Ridhwan <57290483+nabilridhwan@users.noreply.github.com>
2022-06-15 18:24:43 +0800

    feat: :sparkles: Add error middleware handler and error route (to test)

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const authRoute = require("./routes/auth");
 const dataRoute = require("./routes/data");
+const userRoute = require("./routes/user");
 const cors = require("cors");
 const os = require("os");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -15,6 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoute);
 app.use("/data", dataRoute);
+app.use("/user", userRoute);
+
+const uri = "mongodb://root:root@localhost:27017";
+
+mongoose.connect(uri);
 
 app.get("/", (req, res) => {
     const hostname = os.hostname();
@@ -30,6 +38,15 @@ app.get("/info", (req, res) => {
 
 app.get("/test", (req, res) => {
     res.send("This is the test page");
+});
+
+app.get("/error", (req, res, next) => {
+    return next(createError(500, "This is the error page"));
+});
+
+// ! Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).send(err.message);
 });
 
 app.listen(PORT, () => {
