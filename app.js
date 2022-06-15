ab7ab6196a6f80e6030ea6ce3ba1dcdfe0120568
Nabil Ridhwan <57290483+nabilridhwan@users.noreply.github.com>
2022-06-15 18:27:45 +0800

    feat: Add MongoDB atlas URL to uncomment when needed

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use("/data", dataRoute);
 app.use("/user", userRoute);
 
 const uri = "mongodb://root:root@localhost:27017";
+// const uri = "mongodb+srv://Admin:lAf8JiPQynG0mCGm@cluster0.vobnv.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(uri);
 
