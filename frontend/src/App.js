0c913000f3769800922f5c775f9dbf1c250a7ac6
jadenlohh <jadenloh24@gmail.com>
2022-06-15 18:14:13 +0800

    feat: add login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-  
+import Register from './pages/Register';
+
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/login' element={ <Login /> } />
-            </Routes>
-        </Router>
+        <>
+            <div style={{margin:"0",padding:"0"}}>
+                <Router>
+                    <Routes>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                    </Routes>
+                </Router>
+            </div>
+        </>
     );
 }
-  
+
 export default App;
