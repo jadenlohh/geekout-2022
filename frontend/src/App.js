3494ee0cdee4a5e1920e8f6edabb1e9a65bfd2e9
jadenlohh <jadenloh24@gmail.com>
2022-06-16 15:55:47 +0800

    chore: update home and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Test from "./pages/Test";
+import Home  from "./pages/Home";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <div style={{ margin: "0", padding: "0" }}>
                 <Router>
                     <Routes>
+                        <Route path="/" element={<Home />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
                         <Route path="/test" element={<Test />} />
