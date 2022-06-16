14da0ba2f5c61bc3cb16eb2753cabc5fc702e202
Nabil Ridhwan <57290483+nabilridhwan@users.noreply.github.com>
2022-06-16 21:18:58 +0800

    feat: :sparkles: Add Chart and Profile page

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import {
+    Card,
+    Col,
+    Container,
+    FloatingLabel,
+    Form,
+    Row,
+} from "react-bootstrap";
+import { TokenContext } from "../App";
+import NavigationBar from "../components/NavigationBar";
+import useToken from "../hooks/useToken";
+import { useNavigate } from "react-router-dom";
+
+export default function Profile() {
+    const { token } = useContext(TokenContext);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!token) {
+            navigate("/login");
+        }
+
+        const getUserData = async () => {
+            try {
+                const res = await axios.get("http://localhost:3001/user", {
+                    params: {
+                        token,
+                    },
+                });
+
+                const d = res.data.data;
+                const { name: apiName, email: apiEmail } = d;
+
+                setName(apiName);
+                setEmail(apiEmail);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getUserData();
+    }, []);
+
+    return (
+        <div>
+            <NavigationBar />
+            <Container style={{ textAlign: "center" }}>
+                <h1>Profile</h1>
+                <h2>{name}</h2>
+                <FloatingLabel
+                    controlId="name"
+                    className="mb-2"
+                    label="Full Name"
+                    onChange={(e) => setName(e.target.value)}
+                >
+                    <Form.Control
+                        type="text"
+                        className="shadow-none"
+                        placeholder="Your Name"
+                        value={name}
+                        required
+                    />
+                </FloatingLabel>
+
+                <FloatingLabel
+                    controlId="name"
+                    className="mb-2"
+                    label="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                >
+                    <Form.Control
+                        type="text"
+                        className="shadow-none"
+                        placeholder="Your Name"
+                        value={email}
+                        required
+                    />
+                </FloatingLabel>
+            </Container>
+        </div>
+    );
+}
